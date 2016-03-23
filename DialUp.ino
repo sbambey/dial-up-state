79b62a31e4946f5b8a7499d8cbd85179bd3b5ebe
Simon Bambey <sbambey.lp@gmail.com>
2016-03-23 15:16:23 -0700

    initial commit, working rx/tx at 3000micros period

---
+++ DialUp.ino
@@ -0,0 +1,139 @@
+#include "constants.h"
+#include "Arduino.h"
+#include "send.h"
+
+unsigned long last_timestamp;
+unsigned long current_timestamp;
+
+byte current_signal;
+byte received = 0;
+int shift_counter = 0;
+
+String rec = "";
+boolean active = 0;
+
+String transfer_string = "";
+char characters[400];
+
+void send_signature() {
+  /* Signature: 11000101 */
+
+  PORTL |= _BV(0);
+  delayCycle();
+
+  PORTL |= _BV(0);
+  delayCycle();
+
+  PORTL &= ~(_BV(0));
+  delayCycle();
+
+  PORTL &= ~(_BV(0));
+  delayCycle();
+
+  PORTL &= ~(_BV(0));
+  delayCycle();
+  
+  PORTL |= _BV(0);
+  delayCycle();
+
+  PORTL &= ~(_BV(0));
+  delayCycle();
+
+  PORTL |= _BV(0);
+  delayCycle();
+}
+
+/* PIN 53 */
+void setup() {
+  Serial.begin(9600);
+  DDRB = B00000000;
+  DDRL = B11111111;
+
+  last_timestamp = micros();
+}
+
+void loop() {
+
+  while((PINB & B00000001) == current_signal) {
+    
+    if(Serial.available() > 0) {
+      transfer_string = Serial.readString();
+    }
+
+    if(transfer_string.length() > 0 && !active && shift_counter == 0) {
+      transfer_string.toCharArray(characters,400);
+
+      send_signature();
+      
+      for(int i=0; i<transfer_string.length(); i++) {
+        String binary_char = charactersToBinary(String(characters[i]));
+        for(int j=0; j<8;j++) {
+          if(binary_char.charAt(j) == '1') {
+            PORTL |= _BV(0);
+            delayCycle();
+          }
+          else {
+            PORTL &= ~(_BV(0));
+            delayCycle();
+          }
+        }
+      }
+      
+      send_signature();
+      
+      PORTL &= ~(_BV(0));
+      delayCycle();
+      
+      transfer_string = "";
+    }
+  }
+  
+  int multiplier;
+  
+  current_timestamp = micros();
+  
+  if(current_signal == 1) {
+    //Serial.print("1: ");Serial.println(current_timestamp-last_timestamp);
+    multiplier = ((current_timestamp-last_timestamp-CORRECTION_OFFSET)/CYCLE);
+  }
+  else {
+    //Serial.print("0: ");Serial.println(current_timestamp-last_timestamp);
+    multiplier = ((current_timestamp-last_timestamp+CORRECTION_OFFSET)/CYCLE);
+  }
+  //Serial.println(multiplier);
+  //Serial.println(current_signal);
+  if(multiplier < 9) {
+    for(int i=0; i<multiplier; i++) {
+      received = received << 1;
+      received |= current_signal;
+      
+      shift_counter++;
+
+      //Serial.println(shift_counter);
+      
+      if(!active && received == B11000101) {
+        active = 1;
+        shift_counter = 0;
+        Serial.println("Message start");
+      }
+      
+      if(shift_counter == 8) {
+        //Serial.println(received);
+        if(active && received == B11000101) {
+          active = 0;
+          Serial.println("Message end");
+          Serial.println(rec);
+          rec = "";
+          delay(1000);
+        }
+        else if(active) {
+          rec += String((char)received);
+        }
+        shift_counter = 0;
+      }
+    }
+  }
+  
+  current_signal = (PINB & B00000001);
+  last_timestamp = current_timestamp;
+}
---
+++ constants.h
@@ -0,0 +1,8 @@
+#define CYCLE 3000
+#define CORRECTION_OFFSET 48
+
+typedef struct Node {
+  int reading;
+  unsigned long timestamp;
+  Node *next;
+} Node;
